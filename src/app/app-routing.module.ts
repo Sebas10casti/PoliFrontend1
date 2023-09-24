3a96c7315cd700f00375430c970b96e0ece98b18
Sebastian Castiblanco <sebas07casti@gmail.com>
2023-09-24 18:26:51 -0500

    Agregando rutas y nuevos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { ListComponent } from './components/list/list.component';
-import { ListCartComponent } from './components/list-cart/list-cart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ListComponent } from './pages/list/list.component';
+import { ListCartComponent } from './pages/list-cart/list-cart.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'catalogo', pathMatch: 'full' },
+  { path: 'admin/login', component: LoginComponent },
+  { path: 'admin/gestor-archivos', component: LoginComponent },
   { path: 'catalogo', component: ListComponent },
   { path: 'cart', component: ListCartComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { ListComponent } from './components/list/list.component';
 import { TablerIconsModule } from 'angular-tabler-icons';
 import { IconCamera, IconHeart, IconBrandGithub } from 'angular-tabler-icons/icons';
-import { DetailProductComponent } from './components/detail-product/detail-product.component';
-import { ProductComponent } from './components/product/product.component';
-import { ListCartComponent } from './components/list-cart/list-cart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ListComponent } from './pages/list/list.component';
+import { DetailProductComponent } from './pages/detail-product/detail-product.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ListCartComponent } from './pages/list-cart/list-cart.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ClientComponent } from './layout/client/client.component';
+import { AdminComponent } from './layout/admin/admin.component';
 
 
 const icons = {
@@ -26,7 +31,12 @@ const icons = {
     ListComponent,
     DetailProductComponent,
     ProductComponent,
-    ListCartComponent
+    ListCartComponent,
+    NavbarComponent,
+    FooterComponent,
+    SidebarComponent,
+    ClientComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
